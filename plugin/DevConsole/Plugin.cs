84acd882893f9b03fce25961c47d562c98f600f1
Angelo Umali <angelo@theangeloumali.com>
2026-09-18 22:47:45 +0800

    Version 1.0.0 for the first public release

--- plugin/DevConsole/Plugin.cs
+++ plugin/DevConsole/Plugin.cs
@@ -10,7 +10,7 @@ namespace DevConsole
     public sealed class Plugin : BaseUnityPlugin
     {
         public const string Guid = "angelo.el2.devconsole";
-        public const string Version = "0.2.0";
+        public const string Version = "1.0.0";
 
         private State state;
         private Window window;
